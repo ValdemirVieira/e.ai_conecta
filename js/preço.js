983cdcee5e46550662935adbb9d2703e0d4c2360
Luciano Ramos Garcia <lucianorgarcia2009@gmail.com>
2023-03-30 13:52:17 -0300

    alteraçao

---
+++ js/preço.js
@@ -0,0 +1,11 @@
+let ola = document.getElementById('metros');
+
+function receberValor(){
+   var emMetros = document.getElementById("metros").value;
+   console.log(emMetros)
+   var calc = Number(emMetros) * Number(3.5)
+
+   //enviar dados para arquivo utilizando ID
+   document.getElementById("valor_metros").innerHTML = calc
+   
+}
