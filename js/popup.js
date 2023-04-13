703efc672bda816717a06b489496694ab857a174
Luciano Ramos Garcia <lucianorgarcia2009@gmail.com>
2023-04-13 16:15:18 -0300

    criando popup

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,32 @@
-var btn = document.querySelector('.mostrar-esconder');
-var popup = document.querySelector('.popup')
-var informacao = document.querySelector('.email-popup')
+// Seleciona o modal
+var modal = document.getElementById("myModal");
 
+// Seleciona o botão 1
+var btn1 = document.getElementById("btn1");
 
-document.documentElement.onclick = function(event){
-    console.log(event.target)
-    if(event.target == btn){
-        popup.style.display = 'block';
-    }else{
-        popup.style.display = 'none';
-    }
+// Seleciona o botão 2
+var btn2 = document.getElementById("btn2");
+
+// Seleciona o botão de fechar
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Adiciona evento de clique ao botão 1 e ao botão 2
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Quando o usuário clicar no botão de fechar, o modal será fechado
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Quando o usuário clicar em qualquer lugar fora do modal, o modal será fechado
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
